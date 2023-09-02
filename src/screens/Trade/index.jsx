29dee454321292c2b118e2b6028bb1c88bd51e9a
Marcos Willian <markusuuuu@gmail.com>
2023-09-02 04:00:31 -0300

    chore: added last two bottomTabs screens (without nothing inside, only lorem...)

---
+++ src/screens/Trade/index.jsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export function Trade() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat sit est quasi, facere similique incidunt deserunt necessitatibus in! Autem facere quo dolor fugiat enim iure ipsam neque voluptatibus animi deleniti.</Text>
+    </View>
+  )
+}
